cc39994d2841d6de163449c5a0fccbf3b46e7069
Rumman Hasan <jah.hasant@gmail.com>
2022-10-23 14:49:53 +0600

    add desktop navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
+import "./App.scss";
 function App() {
   return (
     <div className="app">
